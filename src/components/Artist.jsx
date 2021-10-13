d8adc04f614af2756f8ee0c32771a4691bc5a864
Elias Rundqvist <rundqvistelias@gmail.com>
2021-10-13 13:10:14 +0200

    done

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
+import '../styles/artist.css'
 
 function Artist() {
 
@@ -34,27 +35,34 @@ useEffect(()=>{
 
     return (
     <div>
+    <h1 className="artistName">
+    {content.name}
+    </h1>
+        <div className="artistWrapper"> 
+        <div className="descriptionWrapper"> 
+        <h2>Description</h2> 
+            <p className="artistDesc">
             
-        <h1>
-           {content.name}
-        </h1>
+            {content.description}
+            </p>
 
-        <p>
-           {content.description}
-        </p>
-
-        <ul>
-            {songs.map(song => (<li key={song.videoId}>{song.name}</li>))} 
+            </div>
             
-        </ul>
-
-        <ul>
-            {albums.map(album => (<li key={album.browseId}>{album.name} 
-                <img src={album.thumbnails[1].url}/>
-            </li>))} 
-            
-        </ul>
-
+            <div className="songListWrapper"> 
+            <ul className="songList">
+            <h2>Songs</h2> 
+                {songs.map(song => (<li key={song.videoId}>{song.name}</li>))} 
+                
+            </ul>
+            </div>
+            <ul className="albumList">
+            <h2>Albums</h2> 
+                {albums.map(album => (<li key={album.browseId}><div className="albumName">{album.name}</div> 
+                    <img style={{width: "70px"}} src={album.thumbnails[1].url}/>
+                </li>))} 
+                
+            </ul>
+        </div>
     </div>
     )
 }
